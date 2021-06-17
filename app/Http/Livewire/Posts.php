52fef7a5e1e6bd1811d1b722bad5d816797cd2d2
Amber <44774806+PrijsAmber@users.noreply.github.com>
2021-06-17 15:20:35 +0200

    edit pagination

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -100,7 +100,7 @@ class Posts extends Component
     public function render()
     {
         return view('livewire.posts', [
-            'posts' => Post::orderBy('created_at', 'DESC')->paginate(5)
+            'posts' => Post::orderBy('created_at', 'DESC')->paginate(10)
         ]);
     }
 }
